bb6dbb389f84e6d62930c065a44ea1b8911a60b6
Vivek Vaishnani <vivek.vaishnani@deltafixes.com>
2024-11-28 15:10:11 +0530

    Header title change as par component

--- src/admin/components/header/header.jsx
+++ src/admin/components/header/header.jsx
@@ -1,14 +1,33 @@
 import { useLocation } from "react-router-dom";
 import "./header.css";
+import { useEffect, useState } from "react";
+
+const titleName = [{ name: "Overview", pathname: "dashboard" },
+{ name: "Orders Details", pathname: "orders" },
+{ name: "Analytics Dashboard", pathname: "analytics" },
+{ name: "Products", pathname: "products" },
+{ name: "Sales Dashboard", pathname: "sales" },
+{ name: "Settings", pathname: "settings" },
+{ name: "Users", pathname: "users" }]
 
 const Header = () => {
   const location = useLocation();
-  // console.log(location);
+  const pathname = location.pathname.replace("/admin/", "");
+
+  const [title, setTitle] = useState("");
+
+  useEffect(() => {
+    titleName.map((item) => {
+      if (item.pathname === pathname) {
+        setTitle(item.name);
+      }
+    })
+  }, [title, pathname]);
 
   return (
     <div className="admin-header-wrapper">
       <div className="header-content">
-        <div className="header-title">Dashboard</div>
+        <div className="header-title">{title}</div>
       </div>
     </div>
   )
